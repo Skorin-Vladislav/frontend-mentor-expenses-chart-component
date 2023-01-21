d6f39cc9bf9b633734f3e7efe7f5007d485ac6ea
Skorin Vladislav <43670004+Skorin-Vladislav@users.noreply.github.com>
2023-01-21 10:32:45 +0300

    header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-    
+      <div className="header">
+        <div className="balance">
+          <p className="balance-txt">My balance</p>
+          <p className="balance-value">$921.48</p>
+        </div>
+        <img className="logo" src={logo} alt="logo"></img>
+      </div>
     </div>
   );
 }
