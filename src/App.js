88ba70841da905b379074cd0cbbca7f5d2dd4cbb
Skorin Vladislav <43670004+Skorin-Vladislav@users.noreply.github.com>
2023-01-21 11:20:29 +0300

    spending footer and header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./images/logo.svg";
+
 import "./App.css";
 
 function App() {
@@ -10,6 +11,24 @@ function App() {
           <p className="balance-value">$921.48</p>
         </div>
         <img className="logo" src={logo} alt="logo"></img>
+      </div>
+
+      <div className="spending-box">
+        <h2 className="spending-txt">Spending - Last 7 days</h2>
+
+        <div className="charts"></div>
+
+        <div className="charts-results">
+          <div className="total">
+            <p className="total-txt">Total this month</p>
+            <p className="total-value">$478.33</p>
+          </div>
+
+          <div className="changes">
+            <p className="change-percent">+2.4%</p>
+            <p className="last-month">from last month</p>
+          </div>
+        </div>
       </div>
     </div>
   );
